165e101f97969324697079918eb776e371b8058d
Jollen Chen <jollen@moko365.com>
2017-07-27 00:04:29 +0800

    update samples

--- main.js
+++ main.js
@@ -17,13 +17,82 @@ app.Message = Backbone.Model.extend({
     },
     wind: {
         speed: -1
-    }
+    },
+    date: '',
+    celsius: 0
+  }
+});
+
+app.Post = Backbone.Model.extend({ 
+  id: null,
+  url: function() {
+        return (this.id === null) ? 'http://localhost:3000/1/post' : 'http://localhost:3000/1/post/' + this.id;
+  },
+  defaults: {
+    posts: []
   }
 });
 
 /**
 * VIEWS
 **/
+app.PostView = Backbone.View.extend({
+    el: '#post',
+    events: {
+        'click .btn-delete': 'delete'
+    },
+    initialize: function() {
+        this.model = new app.Post();
+        this.template = _.template($('#post-tmpl').html());
+
+        this.model.bind('change', this.render, this);
+        this.model.fetch({
+            success: function(model, response, options) {
+            }
+        });
+    },
+    render: function() {
+        var html = this.template(this.model.attributes);
+        this.$el.html(html);
+    },
+    delete: function(e) {
+        var me = this.$el.find(e.target);
+        var id = me.data('post-id');
+        
+        this.model.id = id;
+        this.model.destroy({
+            success: function(model, response, options) {
+                model.id = null;
+            }
+        });
+    }
+});
+
+app.ActionView = Backbone.View.extend({
+    el: '#action',
+    events: {
+        'click .btn-save': 'save'
+    },
+    initialize: function() {
+        this.model = new app.Post();
+    },
+    save: function(e) {
+        e.preventDefault();
+
+        var title = this.$el.find('#subject').val();
+        var content = this.$el.find('#content').val();
+
+        this.model.save({
+            title: title,
+            content: content
+        }, {
+            success: function(model, response, options) {
+                app.postView.model.fetch();
+            }
+        });
+    }
+});
+
 app.MessageView = Backbone.View.extend({
 	el: '#app',
 	events: {
@@ -34,19 +103,20 @@ app.MessageView = Backbone.View.extend({
         this.template = _.template($('#weather-tmpl').html());
 
         this.model.bind('change', this.render, this);
-        this.model.fetch();
-        //this.render();
+        this.model.fetch({
+            success: function(model, response, options) {
+                // Celsius
+                var temp = model.get('main').temp;
+                var celsius = parseInt(temp - 273.15);
+                model.set('celsius', celsius);
+
+                // Date
+                var date = moment().format('LL');
+                model.set('date', date);
+            }
+        });
     },
     render: function() {
-        // Celsius
-        var temp = this.model.get('main').temp;
-        var celsius = parseInt(temp - 273.15);
-        this.model.set('celsius', celsius);
-
-        // Date
-        var date = moment().format('LL');
-        this.model.set('date', date);
-        
         var html = this.template(this.model.attributes);
         this.$el.html(html);
 
@@ -62,7 +132,9 @@ app.MessageView = Backbone.View.extend({
 * BOOTUP
 **/
 $(document).ready(function() {
-	app.messageView = new app.MessageView();
+	//app.messageView = new app.MessageView();
+    app.postView = new app.PostView();
+    app.actionView = new app.ActionView();
 });
 
 }) ();
